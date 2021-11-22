41c123824be7f1cacfb63058c216ca070de4a2b0
Andrey Deriabin <aderiabin.98@gmail.com>
2021-11-22 16:49:34 +0300

    'Config' task

---
+++ practice/config/config.py
@@ -0,0 +1,8 @@
+def repeat(config_file_name):
+    """Вывести повторенную count раз строку str (см. _legend.md)"""
+    pass
+
+
+def get_connection_info(config_file_name, username):
+    """Вывести пару строк (ip, port) для username или для DEFAULT."""
+    pass
