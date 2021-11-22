f8f1bb3f608c287614d680f83fb6193cc21c7f46
Andrey Deriabin <aderiabin.98@gmail.com>
2021-11-22 22:32:36 +0300

    Added JSON task.

---
+++ practice/json/json.py
@@ -0,0 +1,13 @@
+def save_color(color, filename):
+    """Сохранить цвет в файл в формате JSON (см. _legend.md)"""
+    pass
+
+
+def load_color(filename):
+    """Вернуть цвет, записанный в JSON-файле"""
+    pass
+
+
+def links(filename):
+    """Вывести все страницы из JSON-структуры и подсчитать количество ссылок на каждую. (см. legend)"""
+    pass
